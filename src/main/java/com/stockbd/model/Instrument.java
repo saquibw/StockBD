9e154dbb59898a46bc9025c01651f419c666bd45
Saquib <saquibul.waheed@neura-robotics.com>
2024-07-24 18:18:26 +0200

    wip

--- src/main/java/com/stockbd/model/Instrument.java
+++ src/main/java/com/stockbd/model/Instrument.java
@@ -1,9 +1,14 @@
 package com.stockbd.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -20,4 +25,7 @@ public class Instrument {
 	private String code;
 	private String name;
 	private String category;
+	
+	@OneToMany(mappedBy = "instrument", cascade = CascadeType.ALL, orphanRemoval = true)
+	private List<InstrumentPrice> price = new ArrayList<>();
 }
--- src/main/java/com/stockbd/model/InstrumentPrice.java
+++ src/main/java/com/stockbd/model/InstrumentPrice.java
@@ -1,5 +1,33 @@
 package com.stockbd.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class InstrumentPrice {
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private Long id;
+	
+	private double lastTradePrice;
+	private double highPrice;
+	private double lowPrice;
+	private double closePrice;
+	private int tradeCount;
+	private long tradeVolume;
+	
+	@ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "instrument_id")
+    private Instrument instrument;
 
 }
